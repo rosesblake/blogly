bc4d1fd4e08e510e97b2dcb1cdcfab90bf7718a7
rosesblake <rosesblake@yahoo.com>
2024-06-27 10:51:32 -0500

    part 2 of project. Waiting on tests until I have setup help

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #even the example project in course didnt run because of app_context(which was not mentioned), took me hours to figure out.
 from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask, render_template, redirect, request, flash
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -29,7 +29,8 @@ def users_list():
 def get_details(user_id):
     """show more details when clicking a user's name"""
     user = User.query.get_or_404(user_id)
-    return render_template('details.html', user=user)
+    posts = user.user_posts
+    return render_template('details.html', user=user, posts=posts)
 
 @app.route('/create-user', methods=['GET'])
 def create_user():
@@ -79,6 +80,59 @@ def delete_user(user_id):
     db.session.commit()
     return redirect('/')
 
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """display post"""
+    post = db.session.get(Post, post_id)
+    user_id = post.user.id
+    return render_template('post-details.html', post=post, user_id=user_id)
 
+@app.route('/users/<int:user_id>/posts/new')
+def new_post(user_id):
+    """show form to add new post for user"""
+    user = User.query.get_or_404(user_id)
+    return render_template('add-post.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_post(user_id):
+    """make post request with post input fields"""
+    user = User.query.get_or_404(user_id)
+    # Create a new Post object
+    new_post = Post(
+        title=request.form.get('title'),
+        content=request.form.get('content'),
+        user_id=user.id  # Assign the user_id to link the post to the user
+    )
+    
+    # Add the new post to the db
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f'/{user.id}')
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit-post.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def edit_post_submit(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    post.title = request.form.get('title-edit')
+    post.content = request.form.get('content-edit')
+
+    db.session.commit()
+
+    return redirect(f'/posts/{post.id}')
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """Delete the user with the given user_id and redirect to home"""
+    post = Post.query.get_or_404(post_id)
+    user = post.user_id
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(f'/{user}')
 # connect_db(app)
 # db.create_all()
--- models.py
+++ models.py
@@ -10,6 +10,17 @@ class User(db.Model):
     first_name = db.Column(db.String(15), nullable=False)
     last_name = db.Column(db.String(15), nullable=False)
     image_url = db.Column(db.String(), nullable=True)
+    #relationship
+    user_posts = db.relationship('Post', backref='user')
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(25), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 def connect_db(app):
     db.app = app
