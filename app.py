68499da0e86813e5e7bcf9c9d43c01cd8be2ab4e
rosesblake <rosesblake@yahoo.com>
2024-06-29 15:15:09 -0500

    part 3 finished. Still haven't figured out test issue

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #even the example project in course didnt run because of app_context(which was not mentioned), took me hours to figure out.
 from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask, render_template, redirect, request, flash
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 
 app = Flask(__name__)
 
@@ -84,25 +84,33 @@ def delete_user(user_id):
 def show_post(post_id):
     """display post"""
     post = db.session.get(Post, post_id)
+    tags = post.tags
     user_id = post.user.id
-    return render_template('post-details.html', post=post, user_id=user_id)
+    return render_template('post-details.html', post=post, user_id=user_id, tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new')
 def new_post(user_id):
     """show form to add new post for user"""
     user = User.query.get_or_404(user_id)
-    return render_template('add-post.html', user=user)
+    tags = db.session.query(Tag).all()
+    return render_template('add-post.html', user=user, tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new', methods=['POST'])
 def add_post(user_id):
     """make post request with post input fields"""
     user = User.query.get_or_404(user_id)
+    tags_ids = request.form.getlist('tags')
     # Create a new Post object
     new_post = Post(
         title=request.form.get('title'),
         content=request.form.get('content'),
         user_id=user.id  # Assign the user_id to link the post to the user
     )
+    #loop through tag ids and append all tag info to the new post before committing
+    for tag_id in tags_ids:
+        tag = Tag.query.get(tag_id)
+        if tag:
+            new_post.tags.append(tag)
     
     # Add the new post to the db
     db.session.add(new_post)
@@ -112,15 +120,25 @@ def add_post(user_id):
 
 @app.route('/posts/<int:post_id>/edit')
 def edit_post(post_id):
+    """get edit post form and show on page"""
     post = Post.query.get_or_404(post_id)
-    return render_template('edit-post.html', post=post)
+    tags = Tag.query.all()
+    return render_template('edit-post.html', post=post, tags=tags)
 
 @app.route('/posts/<int:post_id>/edit', methods=['POST'])
 def edit_post_submit(post_id):
+    """submit edit to db from edit post form"""
     post = Post.query.get_or_404(post_id)
+    tags_ids = request.form.getlist('tags')
 
     post.title = request.form.get('title-edit')
     post.content = request.form.get('content-edit')
+    #clear current tags and re-loop, then append to post
+    post.tags.clear()
+    for tag_id in tags_ids:
+        tag = Tag.query.get(tag_id)
+        if tag:
+            post.tags.append(tag)
 
     db.session.commit()
 
@@ -134,5 +152,46 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(f'/{user}')
+
+@app.route('/tags')
+def show_tags():
+    """show tag list"""
+    tags = Tag.query.all()
+    return render_template('tags.html', tags=tags)
+
+@app.route('/tags/<int:tag_id>')
+def tag_details(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tag-details.html', tag=tag)
+
+@app.route('/tags/new')
+def create_tag_form():
+    """show create tag form"""
+    return render_template('create-tag.html')
+
+@app.route('/tags/new', methods=['POST'])
+def create_tag_submit():
+    """submit request to add new tag to db"""
+    name = request.form.get('add-tag')
+    new_tag = Tag(name=name)
+    db.session.add(new_tag)
+    db.session.commit()
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/edit')
+def edit_tag_form(tag_id):
+    """show create tag form"""
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('edit-tag.html', tag=tag)
+
+#want to know how i can keep the same list order when I edit the tag, so it doesnt re-append to the end.
+@app.route('//tags/<int:tag_id>/edit', methods=['POST'])
+def edit_tag_submit(tag_id):
+    """submit request to add new tag to db"""
+    tag = db.session.get(Tag, tag_id)
+    name = request.form.get('edit-tag')
+    tag.name = name
+    db.session.commit()
+    return redirect('/tags')
 # connect_db(app)
 # db.create_all()
--- models.py
+++ models.py
@@ -19,8 +19,26 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(25), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    #setup through relationship for tags/posts since posttag has no additional information
+    tags = db.relationship('Tag', secondary='post_tags', backref='post')
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+class PostTag(db.Model):
+    __tablename__ = 'post_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+    def __repr__(self):
+        return f'<PostTag post_id={self.post_id} tag_id={self.tag_id}>'
+
 
 def connect_db(app):
     db.app = app
